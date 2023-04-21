5c04e0834153b4bb76f300e7b1d1147444da35c3
everbird <stephen.zhuang@gmail.com>
2023-04-21 02:23:38 -0700

    OCR time string

--- geek-dad-go_mp42imgs.py
+++ geek-dad-go_mp42imgs.py
@@ -25,22 +25,41 @@ chars = set("".join(ABBR_MONTHS))
 whitelist = digits+"".join(sorted(chars))
 blacklist = "".join(sorted(set(string.ascii_letters) - chars))
 # Set the OCR configuration to recognize only dates in the format "dd/mm/yyyy"
-custom_config = '--oem 3 --psm 11 -c tessedit_char_whitelist={} -c tessedit_char_blacklist={}'.format(whitelist, blacklist)
+date_config = '--oem 3 --psm 11 -c tessedit_char_whitelist={} -c tessedit_char_blacklist={}'.format(whitelist, blacklist)
+time_config = '--oem 3 --psm 11 -c tessedit_char_whitelist=,:APM0123456789 -c tessedit_char_blacklist={}'.format("".join(sorted(set(string.ascii_letters) - set("APM"))))
 
 
-def get_date_string(frame):
+def get_date_string(frame, i):
     x, y = 20, 10
-    w, h = 120, 50
+    w, h = 120, 55
     roi = frame[y:y+h, x:x+w]
+    # Debug
+    # cv2.imwrite(f"images/ds{i}.png", roi)
     gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
     gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
 
-    _text = pytesseract.image_to_string(gray, lang='eng', config=custom_config)
+    _text = pytesseract.image_to_string(gray, lang='eng', config=date_config)
     _text = _text.replace(" ", "")
     text = smart_correct(_text)
     if text != _text:
         print("Correct: {} -> {}".format(_text, text))
     return text.strip()
+
+
+def get_time_string(frame, i):
+    y = 125
+    x = 640
+    w = 160
+    h = 50
+    roi = frame[y:y+h, x:x+w]
+    # Debug
+    # cv2.imwrite(f"images/ts{i}.png", roi)
+    gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+    gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+
+    _text = pytesseract.image_to_string(gray, lang='eng', config=time_config)
+    _text = _text.replace(" ", "")
+    return _text.strip()
 
 
 def smart_correct(text):
@@ -67,6 +86,8 @@ def smart_correct(text):
 
 
 def run():
+    print(date_config)
+    print(time_config)
     video = cv2.VideoCapture('RPReplay_Final1681627796.MP4')
     frame_count = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
 
@@ -90,7 +111,7 @@ def run():
         if 253 <= r <= 255 and 250 <= g <= 253 and 224 <= b <= 247:
             # Crop the frame to the specified region
             cropped_frame = frame[y:y+height, x:x+width]
-            ds = get_date_string(cropped_frame)
+            ds = get_date_string(cropped_frame, i)
             ds = ds.replace(" ", "")
             if len(ds) < 5:
                 print(f"Malformed OCR {ds}. Skipping ...")
@@ -103,7 +124,10 @@ def run():
                 continue
 
             pre = ds
-            cv2.imwrite(f'images/img-frame{i:04d}-{ds}.png', cropped_frame)
+
+            ts = get_time_string(cropped_frame, i)
+            ts = ts.replace(":", "-")
+            cv2.imwrite(f'images/img_frame{i:04d}_{ds}_{ts}.png', cropped_frame)
             # video.set(cv2.CAP_PROP_POS_FRAMES, video.get(cv2.CAP_PROP_POS_FRAMES) + step)
             video.set(cv2.CAP_PROP_POS_FRAMES, video.get(cv2.CAP_PROP_POS_FRAMES) + jump)
             i += jump
@@ -113,5 +137,4 @@ def run():
 
 
 if __name__ == '__main__':
-    print(custom_config)
     run()
