598d52f096a121d8564772a8d0aead1f46247050
everbird <stephen.zhuang@gmail.com>
2023-04-21 01:38:20 -0700

    Add mp4 to images py script

---
+++ geek-dad-go_mp42imgs.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python3
+
+import cv2
+import pytesseract
+import string
+
+
+ABBR_MONTHS = [
+    "JAN",
+    "FEB",
+    "MAR",
+    "APR",
+    "MAY",
+    "JUN",
+    "JUL",
+    "AUG",
+    "SEP",
+    "OCT",
+    "NOV",
+    "DEC",
+]
+
+digits = string.digits
+chars = set("".join(ABBR_MONTHS))
+whitelist = digits+"".join(sorted(chars))
+blacklist = "".join(sorted(set(string.ascii_letters) - chars))
+# Set the OCR configuration to recognize only dates in the format "dd/mm/yyyy"
+custom_config = '--oem 3 --psm 11 -c tessedit_char_whitelist={} -c tessedit_char_blacklist={}'.format(whitelist, blacklist)
+
+
+def get_date_string(frame):
+    x, y = 20, 10
+    w, h = 120, 50
+    roi = frame[y:y+h, x:x+w]
+    gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+    gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+
+    _text = pytesseract.image_to_string(gray, lang='eng', config=custom_config)
+    _text = _text.replace(" ", "")
+    text = smart_correct(_text)
+    if text != _text:
+        print("Correct: {} -> {}".format(_text, text))
+    return text.strip()
+
+
+def smart_correct(text):
+    corrections = {
+        "O": "0",
+        "L": "1",
+        "R": "2",
+        "E": "3",
+        "A": "4",
+        "S": "5",
+        "G": "6",
+        "T": "7",
+        "B": "8",
+    }
+    month = text[:3]
+    day = text[3:]
+    r = ""
+    for ch in day:
+        if ch in corrections:
+            r += corrections[ch]
+        else:
+            r += ch
+    return "{}{}".format(month, r)
+
+
+def run():
+    video = cv2.VideoCapture('RPReplay_Final1681627796.MP4')
+    frame_count = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+
+    step = 1
+    jump = 40
+    px = 30
+    py = 30
+
+    i = 0
+    pre = None
+    while i < frame_count:
+        ret, frame = video.read()
+        # Define the region to crop
+        x = 30
+        y = 260
+        width = 830
+        height = 1400
+
+        r,g,b = frame[py+y, px+x, 2], frame[py+y, px+x, 1], frame[py+y, px+x, 0]
+
+        if 253 <= r <= 255 and 250 <= g <= 253 and 224 <= b <= 247:
+            # Crop the frame to the specified region
+            cropped_frame = frame[y:y+height, x:x+width]
+            ds = get_date_string(cropped_frame)
+            ds = ds.replace(" ", "")
+            if len(ds) < 5:
+                print(f"Malformed OCR {ds}. Skipping ...")
+                i += 1
+                continue
+
+            if pre == ds:
+                print(f"Still the same date: {ds}")
+                i += jump
+                continue
+
+            pre = ds
+            cv2.imwrite(f'images/img-frame{i:04d}-{ds}.png', cropped_frame)
+            # video.set(cv2.CAP_PROP_POS_FRAMES, video.get(cv2.CAP_PROP_POS_FRAMES) + step)
+            video.set(cv2.CAP_PROP_POS_FRAMES, video.get(cv2.CAP_PROP_POS_FRAMES) + jump)
+            i += jump
+        else:
+            i += 1
+
+
+
+if __name__ == '__main__':
+    print(custom_config)
+    run()
